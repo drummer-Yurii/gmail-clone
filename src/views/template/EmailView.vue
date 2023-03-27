9a6bd9040ca441154818e59ce3e5f04676a9cc96
Yurii Rybachok <drummer2009@ukr.net>
2023-03-27 19:18:46 +0300

    create Icon component

--- src/views/template/EmailView.vue
+++ src/views/template/EmailView.vue
@@ -2,8 +2,19 @@
     <div id="EmailView" class="bg-gray-50 h-screen">
         <div class="flex items-center p-2">
             <div class="flex justify-start items-center px-3.5">
-
+                <IconComponent 
+                    iconString="menu"
+                    iconColor="#636363"
+                    :iconSize="19"
+                    hoverColor="hover:bg-gray-200"
+                    text="Main menu"
+                    class="-ml-2 mr-2"
+                />
             </div>
         </div>
     </div>
 </template>
+
+<script setup>
+import IconComponent from '@/components/IconComponent.vue';
+</script>

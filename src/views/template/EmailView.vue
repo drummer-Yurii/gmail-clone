40b4a89da98479ac9d78d5b82a0a2ac1aafe7e94
Yurii Rybachok <drummer2009@ukr.net>
2023-03-27 18:16:35 +0300

    preparing the project

---
+++ src/views/template/EmailView.vue
@@ -0,0 +1,9 @@
+<template>
+    <div id="EmailView" class="bg-gray-50 h-screen">
+        <div class="flex items-center p-2">
+            <div class="flex justify-start items-center px-3.5">
+
+            </div>
+        </div>
+    </div>
+</template>
